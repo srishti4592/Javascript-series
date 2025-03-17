5825c982a201b9ada16ecbaa6330a097d3a15f94
srishti4592 <162158118+srishti4592@users.noreply.github.com>
2025-03-17 09:43:42 +0000

    datatypes

---
+++ jspractice/02_datatypes.js
@@ -0,0 +1,15 @@
+let name="Srishti" //String
+let age=23 //numeric
+let isLoggedin= false //Boolean
+
+
+// Primitive Datatype
+// number => 2 to powe 53
+// bigint
+// string=> " "
+// boolean=> true/false
+// null => standalone value
+// undefined=>
+// symbol=> unique
+
+console.log(typeof age)
